789df94a67c871b2bb5cfb1146e68292e22693dd
syedmaax <rehurehana85@gmail.com>
2024-11-07 23:01:51 +0530

    LongestSequence

--- src/Karustaba43.java
+++ src/Karustaba43.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
-public class Karustaba43 {
+class Karustaba43 {
+    public String multiply(String num1, String num2) {
+        int [] arr = new int[num1.length()+num2.length()];
+        for(int i=num1.length()-1;i>=0;i--){
+            for(int j = num2.length()-1;j>=0;j--){
+                int mul = (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                int x = i+j;
+                int y = i+j+1;
+                int sum = mul+arr[y];
+                arr[x]+=sum/10;
+                arr[y]=sum%10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i:arr){
+            if(!(sb.isEmpty() &&i==0)){
+                sb.append(i);
+            }
+        }
+        return sb.isEmpty() ?"0":sb.toString();
+    }
 }
--- src/LongestSequencce.java
+++ src/LongestSequencce.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
-public class LongestSequencce {
+class LongestSequence {
+    public int longestOnes(int[] nums, int k) {
+        int i=0;
+        int flips=0;
+        int max=Integer.MIN_VALUE;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]==0){
+                flips++;
+            }
+            while(flips>k){
+                if(nums[i]==0){
+                    flips--;
+                }
+                i++;
+            }
+            max=Math.max(j-i+1,max);
+        }
+        return max;
+    }
 }
