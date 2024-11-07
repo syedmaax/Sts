ec540f8ad5df5a45073d1faf41f2ded64cf847da
syedmaax <rehurehana85@gmail.com>
2024-11-07 23:03:46 +0530

    BlockSwap

---
+++ src/BlockSwap189.java
@@ -0,0 +1,12 @@
+class BlockSwap189 {
+    public void rotate(int[] nums, int k) {
+        int [] out = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            out[(i+k)%nums.length]=nums[i];
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=out[i];
+        }
+
+    }
+}
