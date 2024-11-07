9b7f924b7a5768d8aa8213b2f06dc69fb20e2ffd
syedmaax <rehurehana85@gmail.com>
2024-11-08 00:47:58 +0530

    Josephus Trap

---
+++ src/JosephusTrap1823.java
@@ -0,0 +1,9 @@
+class JosephusTrap1823 {
+    public int findTheWinner(int n, int k) {
+
+        if(n==1){
+            return 1;
+        }
+        return (findTheWinner(n-1,k)+k-1)%n+1;
+    }
+}
